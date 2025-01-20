9723b32375393c568eab10d06a5c976dc7c2f12f
AdrianPRG <alopgal962@g.educaand.es>
2025-01-20 12:19:25 +0100

    Solucion de errores

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-/**
- * Tres formas de almacenar valores en memoria en javascript:
- *      let: se puede modificar
- *      var: se puede modificar
- *      const: es constante y no se puede modificar
- */
+// Indicamos el puerto en el que vamos a desplegar la aplicación
+const port = process.env.PORT || 8081;
 
-// Importamos las bibliotecas necesarias.
-// Concretamente el framework express.
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+//URL de conexion
+const uri = "mongodb+srv://alopgal962:a3SUqGNvkvwFZuep@cluster0.bovi8.mongodb.net/Dawebconcesionario?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API versio
 const express = require("express");
 
 // Inicializamos la aplicación
@@ -15,51 +13,29 @@ const app = express();
 // Indicamos que la aplicación puede recibir JSON (API Rest)
 app.use(express.json());
 
-// Indicamos el puerto en el que vamos a desplegar la aplicación
-const port = process.env.PORT || 8081;
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-//URL de conexion
-
-const uri = "mongodb+srv://alopgal962:basedaweb962@cluster0.bovi8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 //Creamos el cliente con las opcio es de mongoDb del objeto de la api. 
-
-const client = new MongoClient(uri, {
+const cliente = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: false,
     deprecationErrors: true,
   }
 });
 
+
 let db;
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-
-    db = await client.db("Dawebconcesionario");
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    //Ponemos aqui el codigo de la api
-  } catch (e){
-    // Ensures that the client will close when you finish/error
-    console.log(e.pri);
-  }
-}
-
-run().catch(console.dir);
-
 // Arrancamos la aplicación
+
 app.listen(port, async () => {
-   // Send a ping to confirm a successful connection
-   console.log(`Servidor desplegado en puerto: ${port}`);
+  console.log(`Servidor desplegado en puerto: ${port}`);
+  try{
+    await cliente.connect();
+    db = await cliente.db("Dawebconcesionario");
+    console.log("Conexión al cliente de MongoDB exitosa.");
+  }catch{
+    console.log("Error");
+  }
 });
 
 
@@ -72,47 +48,65 @@ app.get("/concesionarios", async (request, response) => {
 
 // Obtener un solo concesionario
 app.get("/concesionarios/:id", async (request, response) => {
-  const id = request.params.id;
-  let result = await db.collection("Concesionarios").findOne({"_id": new ObjectId(id)});
-  response.json({ result });
+  try{
+    const id = request.params.id;
+    let result = await db.collection("Concesionarios").findOne({"_id": new ObjectId(id)});
+    response.json({ result });
+  }catch{
+    response.json("No hay datos que concuerden");
+  }
 });
 
 // Obtener todos los coches de un concesionario
 
 app.get("/concesionarios/:id/coches", async (request, response) => {
-  const id = request.params.id;
-  //Con projection decimos que no aparezca id pero si listado
-  let result = await db.collection("Concesionarios").findOne({"_id": new ObjectId(id)},{projection : {"listado":1, "_id":0}});
-  response.json({ result });
+    try{
+        const id = request.params.id;
+      //Con projection decimos que no aparezca id pero si listado
+      let result = await db.collection("Concesionarios").findOne({"_id": new ObjectId(id)},{projection : {"listado":1, "_id":0}});
+      response.json({ result });
+    }catch{
+      response.json("No hay datos que concuerden");
+    }
 });
 
 //Obtener un coche especifico de un concesionario
 
 app.get("/concesionarios/:id/coches/:id2", async (request, response) => {
-  const id = request.params.id;
-  const id2 = request.params.id2;
-  //Con projection decimos que no aparezca id pero si listado
-  let result = await db.collection("Concesionarios").findOne({"_id": new ObjectId(id)},{projection : {"listado":1, "_id":0}});
-  result = result.listado[id2];
-  response.json({ result });
+  try{
+      const id = request.params.id;
+    const id2 = request.params.id2;
+    //Con projection decimos que no aparezca id pero si listado
+    let result = await db.collection("Concesionarios").findOne({"_id": new ObjectId(id)},{projection : {"listado":1, "_id":0}});
+    result = result.listado[id2];
+    response.json({ result });
+  }
+  catch{
+    response.json("No hay datos que concuerden");
+  }
 });
+
 
 /*
 // INSERCCION DE DATOS
 
 // Añadir un nuevo concesionario
-app.post("/concesionarios", (request, response) => {
-  concesionarios.push(request.body);
-  response.json({ message: "ok" });
+app.post("/concesionarios", async (request, response) => {
+  await db.collection("Concesionarios").insertOne(request.body);
+  response.json({ message: "Concesionario añadido con exito" });
 });
 
 //Añadir un coche a un concesionario
 
-app.post("/concesionarios/:id/coches", (request, response) => {
-  const id = request.params.id;
-  concesionarios[id]["listado"].push(request.body);
+app.post("/concesionarios/:id/coches", async (request, response) => {
+  let id = request.params.id;
+  let concesionario =  await db.collection("Concesionarios").findOneAndUpdate({"_id": new ObjectId(id)},{"listado":request.body});
   response.json({ message: "ok" });
 });
+
+
+/*
+
 
 //ACTUALIZACION DE DATOS
 
